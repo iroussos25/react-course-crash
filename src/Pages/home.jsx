055b073460a51b22e193c32e0f1ed38ba8273af8
Yiannis <ioannis.roussos@hotmail.com>
2025-11-01 14:34:13 +0200

    Almost had it.

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,14 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import User from "../Components/user.jsx";
 
 function Home() {
+    const [users,  setUsers] = useState([])
+    async function fetchUsers() {
+        const {data} = await axios.get("https://jsonplaceholder.typicode.com/users")
+        setUsers(data);
+    }
+    
     useEffect(() => {
-        async function main() {
-        console.log(axios.get("https://jsonplaceholder.typicode.com/users"))
-        }
-        main();
+        setTimeout(() => {
+            
+            fetchUsers();
+        }, 500);
     }, []);
-    return <h1> Home </h1>;
-}
+
+const pixels = "3px";
+
+    return (
+     <div>
+
+        {users.map((user)=> {
+        <User id={user.id} name={user.name} email={user.email} username={user.username}/>
+      })}
+      </div>
+    )
+    }
 
 export default Home;
